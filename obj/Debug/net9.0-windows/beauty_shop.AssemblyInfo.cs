28c1748afa7dd39624d5f58c7bb6da7d00aef4ae
Kieuu-kieu <chukieu2308@gmail.com>
2025-05-30 23:33:57 +0700

    m

--- obj/Debug/net9.0-windows/beauty_shop.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/beauty_shop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("beauty_shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ba40a6bc5f406e1549a788f1eb121e4b9c29e38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3caf80ed09133ff67ef5037d263d9f7faaf952d")]
 [assembly: System.Reflection.AssemblyProductAttribute("beauty_shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("beauty_shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
